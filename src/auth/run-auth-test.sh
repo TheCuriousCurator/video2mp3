48acdb9ec99fadb349f62c3416a6f3ee10afb586
TheCuriousCurator <curious.curator.services@gmail.com>
2025-11-02 10:17:57 +0530

    add curl commands

---
+++ src/auth/run-auth-test.sh
@@ -0,0 +1,3 @@
+curl -X POST -H "Content-Type: application/json" -u "email@gmail.com:Admin123" http://127.0.0.1:5000/login > token.txt
+curl -X POST -H "Authorization: Bearer $(cat token.txt)" http://127.0.0.1:5000/validate
+rm token.txt
